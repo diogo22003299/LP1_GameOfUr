dfdce7c2d5082d27da422fcc31c5ae2c914844f6
Lumy22002335 <blndogsun@hotmail.com>
2021-05-16 19:31:31 +0100

    Added the dice (Dice.cs) and the pieces (Piece.cs)

---
+++ RoyalGameOfUr/Dice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Royal_Game_of_Ur
+{
+    class Dice
+    {
+        // Declare a new Random
+        private readonly Random rand;
+
+        // The number of dice
+        private readonly int numDice;
+
+        /// <summary>
+        /// Dice Constructor
+        /// </summary>
+        public Dice()
+        {
+            rand = new Random();
+            numDice = 4;
+        }
+
+        /// <summary>
+        /// Roll The dices with chance based of the original game
+        /// </summary>
+        /// <returns>Rolled Value</returns>
+        public int RollDice()
+        {
+            int roll = 0;
+
+            for (int i = 0; i < numDice; i++)
+            {
+                roll += rand.NextDouble() > 0.5f ? 1 : 0;
+            }
+
+            return roll;
+        }
+    }
+}
---
+++ RoyalGameOfUr/Piece.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Royal_Game_of_Ur
+{
+    public class Piece
+    {
+        public int PlayerId { get; private set; }
+
+        public char Sprite { get; private set; }
+
+        public int InGameHouse { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Piece(int id)
+        {
+            PlayerId = id;
+
+            Sprite = PlayerId == 1 ? 'O' : 'T';
+
+            InGameHouse = 0;
+        }
+    }
+}
